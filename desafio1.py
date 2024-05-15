4119d3dc4720e40d4012acc58cd05fe9f5cf5783
Rodolfo Volpiani <rodolfovolpiani@outlook.com>
2024-05-15 19:13:41 -0300

    Desafio 1 de python para IA

---
+++ desafio1.py
@@ -0,0 +1,66 @@
+menu = """
+
+[d] depositar
+[s] sacar
+[e] extrato
+[q] sair
+
+"""
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "d":
+        valor = float(input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou. O valor informado é inválido.")
+
+    elif opcao == "s":
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > limite
+
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou. Você não tem saldo suficiente.")
+
+        elif excedeu_limite:
+            print("Operação falhou. O valor do saque excede o limite.")
+
+        elif excedeu_saques:
+            print("Operação falhou. Numero máximo de saques excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}"
+            numero_saques += 1
+
+        else:
+            print("Operação falhou. O valor informado é inválido.")
+
+    elif opcao == "e":
+        print("\n================= EXTRATO =================")
+        print("\nNão foram realizadas movimentações" if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("\n===========================================")
+
+    elif opcao == "q":
+        break
+
+    else:
+        print("Operação inválida. Por favor selecione novamente a operação desejada")
